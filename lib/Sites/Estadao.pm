d91abd3621efbef7531e48c65fb0243a5b770109
hernan lopes <hernanlopes@gmail.com>
2011-08-19 00:18:09 -0300

    added new site for examples.

---
+++ lib/Sites/Estadao.pm
@@ -0,0 +1,46 @@
+package Sites::Estadao; #is UTF-8 charset
+use Moose;
+with qw(Jungle::Spider);
+
+has startpage => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'http://www.estadao.com.br/ultimas/',
+);
+
+sub on_start { 
+    my ( $self ) = @_; 
+    $self->append( search => $self->startpage );
+}
+
+sub search {
+    my ( $self ) = @_; 
+    my $news = $self->tree->findnodes( '//ul/li/h2/a' );
+    foreach my $item ( $news->get_nodelist ) {
+         my $url = $item->attr( 'href' );
+         $self->prepend( detail_noticia => $url ); #  append url on end of list
+    }
+}
+
+sub on_link {
+    my ( $self, $url ) = @_;
+    if ( $url =~ m{pagina\.php\?i=(1|2)$}ig ) {
+         $self->prepend( search => $url ); #  append url on end of list
+    }
+}
+
+sub detail_noticia {
+    my ( $self ) = @_; 
+    my $content_nodes = $self->tree->findnodes( '//div[@class="texto-noticia"]//div[@class="corpo"]//p' );
+    my $content = '';
+    foreach my $node ( $content_nodes->get_nodelist ) {
+        $content .= $node->as_text. "\n";
+    }
+    $self->data->author( $self->tree->findvalue( '//div[@class="bb-md-noticia-autor"]' ) );
+    $self->data->webpage( $self->current_page );
+    $self->data->content( $content );
+    $self->data->title( $self->tree->findvalue( '//title' ) );
+    $self->data->save;
+}
+
+1;
