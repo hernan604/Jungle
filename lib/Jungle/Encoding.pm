42275e194eb752c959f8aa1656ff49d8ed329376
hernan lopes <hernanlopes@gmail.com>
2011-08-18 23:14:42 -0300

    fixed utf8 detection bug... should be ok now

--- lib/Jungle/Encoding.pm
+++ lib/Jungle/Encoding.pm
@@ -1,13 +1,14 @@
 package Jungle::Encoding;
 use Encode::Guess;
 use Moose::Role;
+use Encode; 
 use utf8;
 
 sub safe_utf8 {
     my ( $self, $content ) = @_;
     Encode::Guess->add_suspects(qw/iso-8859-1 utf8/);
     my $decoder = Encode::Guess->guess($content);
-    return $content unless ref($decoder);
+    return decode_utf8($content) unless ref($decoder);
     my $content_utf8 = $decoder->decode($content);
     return $content_utf8;
 }
