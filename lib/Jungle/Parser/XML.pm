ca3b2bf2d026ebc9bae7932152a4c2514f8ed004
hernan lopes <hernanlopes@gmail.com>
2011-08-23 12:54:05 -0300

    renamed parser files

---
+++ lib/Jungle/Parser/XML.pm
@@ -0,0 +1,16 @@
+package Jungle::Parser::XML;
+use Moose::Role;
+use XML::XPath;
+
+has xml => (
+    is  => 'rw',
+    isa => 'Any',
+);
+
+sub parse_xml {
+    my ($self) = @_;
+    my $xml = XML::XPath->new( xml => $self->html_content );
+    $self->xml( $xml );
+}
+
+1;
